a7abe6cd2463341ae4888de3a90a7c43526be719
Ace Abadenis <shozuabadenis@gmail.com>
2026-02-25 22:51:53 +0800

    a

--- resources/views/finance/disbursement.blade.php
+++ resources/views/finance/disbursement.blade.php
@@ -64,14 +64,16 @@
                                     @endif
 
                                     {{-- Delete --}}
-                                    <!-- <form method="POST" action="{{ route('disbursements.destroy', $item->id) }}"
-                                          onsubmit="return confirm('Delete this record?')" class="d-inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="bg-transparent border-0 p-0 text-danger" title="Delete">
-                                            <i class="fe fe-trash fe-18"></i>
-                                        </button>
-                                    </form> -->
+                                    @if(!empty($item->id))
+                                        {{--- <form method="POST" action="{{ route('disbursements.destroy', $item->id) }}"
+                                              onsubmit="return confirm('Delete this record?')" class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="bg-transparent border-0 p-0 text-danger" title="Delete">
+                                                <i class="fe fe-trash fe-18"></i>
+                                            </button>
+                                        </form> ---}}
+                                    @endif
                                 </div>
                             </td>
                         </tr>
